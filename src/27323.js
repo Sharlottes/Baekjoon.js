e2cae472ea445747d79d5a3dbb8d7f5cf3965969
Sharlotte <aaa9810321@gmail.com>
2023-03-06 14:30:13 +0900

    27323

---
+++ src/27323.js
@@ -0,0 +1,15 @@
+const rl = require("readline").createInterface(process.stdin, process.stdout);
+let lc = 0, a, b;
+rl.on("line", (line) => {
+  lc++;
+  switch(lc) {
+    case 1:
+      a = +line;
+      break;
+    case 2:
+      b = +line;
+      console.log(a * b);
+      rl.close();
+      break;
+  }
+});

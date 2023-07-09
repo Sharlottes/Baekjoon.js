201a13150c33f48f51fc94dbf156620337d1c13a
Sharlotte <seeseafood@naver.com>
2023-07-09 15:54:34 +0900

    day6

--- src/1000.js
+++ src/1000.js
@@ -1,9 +1,4 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const rl = require("readline").createInterface(process.stdin, process.stdout);
 
 rl.on("line", function (line) {
   let [a, b] = line.split(/\s/g).map((e) => parseInt(e));
